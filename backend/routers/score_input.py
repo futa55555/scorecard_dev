248583d1092fb346bd3ebc7735e1f6e4e69a19be
Futa Yamakawa <futa.ymkw@gmail.com>
2025-08-13 03:00:20 +0900

    不要なコメントアウト削除

--- backend/routers/score_input.py
+++ backend/routers/score_input.py
@@ -19,41 +19,6 @@ def start_game(game_id: int, db: Session = Depends(get_db)):
     return service.game_start(db, game_id)
 
 
-# @router.get("/api/games/{game_id}/all_atbats", response_model=schema.StateWithAtBats)
-# def get_all_atbats(game_id: int, db: Session = Depends(get_db)):
-#     """
-#     指定試合の全AtBat + PitchEvent + AdvanceEventを返す
-#     """
-#     atbats = (
-#         db.query(models.AtBat)
-#         .filter(models.AtBat.game_id == game_id)
-#         .options(
-#             joinedload(models.AtBat.pitch_events)  # AtBat → PitchEvent
-#             .joinedload(models.PitchEvent.advance_events)  # PitchEvent → AdvanceEvent
-#         )
-#         .all()
-#     )
-    
-#     state = service.get_game_state(db, game_id)
-    
-#     return {"state": state, "atbats": atbats}
-    
-    
-# @router.post("/api/score-input/{game_id}", response_model=schema.ScoreInputResponse)
-# def post_score_input(game_id: int, input_data: schema.ScoreInputSchema, db: Session = Depends(get_db)):
-#     """
-#     投球を受け取り、PitchEvent作成 + 進塁候補サジェスト
-#     """
-#     response = service.record_pitch_event(db, game_id, input_data)
-    
-#     return response
-
-
-# @router.post("/api/score-input/{game_id}/confirm")
-# def confirm_score_input(game_id: int, input_data: schema.ConfirmScoreInput, db: Session = Depends(get_db)):
-#     response = service.confirm_score_input(db, game_id, input_data)
-#     return response
-
 @router.get("/api/games/{game_id}/state_all_innings", response_model=schema.StateWithInnings)
 def get_state_and_all_atbats(
     game_id: int,
